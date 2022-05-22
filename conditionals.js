326af0846a01d85de5d493948679b94d8de553ee
Liana Baciu <lia@lianabaciu.com>
2022-05-22 13:27:14 -0400

    adding ternany operator

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,9 @@
-const apples = 2;
+const apples = 17;
 if (apples < 5) {
     console.log('I should go buy more apples.');
 } else {
     console.log('I have enough apples.');
 }
+
+const message = (apples < 5) ? 'I should go buy more apples.' : 'I have enough apples.';
+console.log(message);
