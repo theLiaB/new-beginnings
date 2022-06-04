b5697e7568646d23fddb9ec5564fac1e800b8b3f
Liana Baciu <lia@lianabaciu.com>
2022-06-03 20:25:36 -0400

    on 6.301

--- 06-strings-exercises.js
+++ 06-strings-exercises.js
@@ -80,7 +80,13 @@ console.log("1 is not in the alphabet:", inAlphabet("1") === false);
 returns true if it is included in the following string, otherwise false.
 The following lines should help test if your function works correctly. They should print true.*/
 const digits = "1234567890";
-
+const isDigit = (testString, inThisString) => {
+    if (alphabet.includes(isInAlphabestInput)) {
+        return true;
+    } else {
+        return false;
+    }
+};
 // console.log('1 is a digit:', isDigit('1') === true);
 // console.log('9 is a digit:', isDigit('9') === true);
 // console.log('a is not a digit:', isDigit('a') === false);
