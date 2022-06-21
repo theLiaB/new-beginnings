bb9fa5373f6362a82826986b2c0ac963af202b08
Liana Baciu <lia@lianabaciu.com>
2022-06-20 20:01:38 -0400

    7.43

--- testfile.js
+++ testfile.js
@@ -1,4 +1,17 @@
-const hasNull = (array) => (array.includes(null) ? true : false);
+const badMatrix = (array) =>
+    array[0].includes(null) | array[1].includes(null) | array[2].includes(null) ?
+    true :
+    false;
 
-const noRay2 = [null, "5", 8];
-console.log("noRay2 has null in it", hasNull(noRay2) === true);
+const weeMatrix = [
+    [45, 39, 89, 67, 7, 71, 85, 44, 47, 39],
+    [89, 32, 81, 19, 43, 17, 47, 10, 6, 58],
+    [8, 59, 79, 3, 14, 21, 83, 18, 27, 49],
+];
+const nullriffic = [
+    [45, 39, 89, 67, null],
+    [89, 32, 81, 19, null, 47, 10, 6, 58],
+    [8, 59, 79, 3, 14, 21, 83, 18, 27, 49],
+];
+console.log("weeMatrix has no null in it", badMatrix(weeMatrix) === false);
+console.log("nullriffic has null in it", badMatrix(nullriffic) === true);
